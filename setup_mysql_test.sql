671e7cdd88faa8d68cdd0ae1e01090ac3b7d5e22
tundelakins <tundelakins4real@yahoo.com>
2022-09-14 09:04:50 +0000

    AirBnB clone - MySQL

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Create dev database and user
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
