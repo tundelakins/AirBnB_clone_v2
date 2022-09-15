07279427bb3db84e49c0fa8aebab2bb94b3e2779
ritatechie <rita.nwaodu@gmail.com>
2022-09-15 11:46:44 -0700

    setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,8 @@
--- Create dev database and user
+-- Prepares a MySQL Test server for the project.
+-- create project testing database with the name : hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
